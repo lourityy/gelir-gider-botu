5a7f01dcf41b2bc5f3f44cec8f82d699b4069f2b
lourity <96919081+lourityy@users.noreply.github.com>
2023-04-29 13:49:27 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+// Discord
+const { PermissionsBitField, EmbedBuilder, ButtonStyle, Client, GatewayIntentBits, ChannelType, Partials, ActionRowBuilder, SelectMenuBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, InteractionType, SelectMenuInteraction, ButtonBuilder, AuditLogEvent } = require("discord.js");
+const Discord = require("discord.js");
+const fs = require("fs")
+// İNTENTS
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.MessageContent], shards: "auto", partials: [Partials.Message, Partials.Channel, Partials.GuildMember, Partials.Reaction, Partials.GuildScheduledEvent, Partials.User, Partials.ThreadMember] });
+const PARTIALS = Object.values(Partials);
+// Database
+const db = require("croxydb")
+// discord.gg/altyapilar - Lourity
+
+global.client = client;
+client.commands = (global.commands = []);
+const { readdirSync } = require("fs")
+const { TOKEN } = require("./config.json");
+readdirSync('./commands').forEach(f => {
+    if (!f.endsWith(".js")) return;
+
+    const props = require(`./commands/${f}`);
+
+    client.commands.push({
+        name: props.name.toLowerCase(),
+        description: props.description,
+        options: props.options,
+        dm_permission: false,
+        type: 1
+    });
+
+    console.log(`[COMMAND] ${props.name} komutu yüklendi.`)
+
+});
+readdirSync('./events').forEach(e => {
+
+    const eve = require(`./events/${e}`);
+    const name = e.split(".")[0];
+
+    client.on(name, (...args) => {
+        eve(client, ...args)
+    });
+    console.log(`[EVENT] ${name} eventi yüklendi.`)
+});
+
+
+client.login(TOKEN)
+
+// Bir Hata Oluştu
+process.on("unhandledRejection", (reason, p) => {
+    console.log(reason, p);
+})
+// Hata oluştuğunda botun kapanmamasını sağlar
+process.on("unhandledRejection", async (error) => {
+    return console.log("Bir hata oluştu! " + error)
+})
+//
+//
+// discord.gg/altyapilar - Lourity
